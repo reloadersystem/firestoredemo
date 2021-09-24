ea8a7d9de688462e94cdaf9d9c38eec9d1b5ac4d
reloader <37155445+reloadersystem@users.noreply.github.com>
2021-09-24 11:44:29 -0500

    Initial commit

---
+++ app/src/main/java/com/reloader/firestoredemo/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.reloader.firestoredemo
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val db = FirebaseFirestore.getInstance()
+
+        //consultar  imformacion
+        db.collection("ciudades").document("NY").get().addOnSuccessListener { document ->
+            document?.let {
+//                Log.d("Firebase", "Document da: ${document.data}")
+
+                val ciudad = document.toObject(Ciudad::class.java)
+
+//                val color = document.getString("color")
+//                val population = document.get("population").toString()
+//                Log.d("Firebase", "Color: ${color}")
+//                Log.d("Firebase", "population : ${population}")
+
+                Log.d("Firebase", "Color: ${ciudad?.color}")
+                Log.d("Firebase", "population : ${ciudad?.population}")
+                Log.d("Firebase", "PostalCode : ${ciudad?.pc}")
+
+
+            }
+        }.addOnFailureListener { error ->
+            Log.e("FirebaseError", error.toString())
+        }
+
+        //Ingresasr informacion
+        db.collection("ciudades").document("LA").set(Ciudad(52200, "Red")).addOnSuccessListener {
+            Log.d("Firebase", "Se guardo la ciudad correctamente")
+        }.addOnFailureListener { error ->
+            Log.e("FirebaseError", error.toString())
+        }
+
+    }
+
+}
+
+data class Ciudad(
+    val population: Int = 0,
+    val color: String = "",
+    val pc: Int = 0
+)
+
